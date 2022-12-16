6787938abdb537d99dbb8184cca275d96d03f275
Dauthlin <70644516+Dauthlin@users.noreply.github.com>
2022-12-16 15:12:58 +0000

    basic criteria in

--- src/components/ProfileForm.js
+++ src/components/ProfileForm.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import { Field, reduxForm } from "redux-form";
-import { Form, Message } from "semantic-ui-react";
+import {Form, Header, Message} from "semantic-ui-react";
 
 const renderCheckbox = field => (
   <Form.Checkbox
@@ -39,92 +39,499 @@ const renderTextArea = field => (
   />
 );
 
+
+
 const ProfileForm = props => {
   const { handleSubmit, reset } = props;
 
   return (
     <Fragment>
-      <Message info>
-        <p>
-          You will don't need any special mappings for <code>Form.Input</code>,
-          because it passed events from native inputs.
-        </p>
-
-        <p>
-          The situation with other components is more complicated, because the{" "}
-          <code>Field</code> relies on the native events. However, it can be
-          easily with{" "}
-          <a
-            href="https://redux-form.com/7.4.2/docs/api/field.md/#2-a-stateless-function"
-            target="_blank"
-          >
-            stateless function
-          </a>. We recomend to wrap them with generic components to reduce forms
-          complexivity.
-        </p>
-      </Message>
-
       <Form onSubmit={handleSubmit}>
-        <Form.Group widths="equal">
-          <Field
+        <Field
             component={Form.Input}
-            label="First name"
-            name="firstName"
-            placeholder="First name"
+            label="Minimum team size"
+            name="size_of_teams"
+            placeholder="Minimum team size"
+        />
+        <Header as='h2'>Criteria</Header>
+        <Header as='h3'>Diversity</Header>
+        <Form.Group inline>
+          <Field
+              component={renderCheckbox}
+              label="Average"
+              name="diversity_average"
+          />
+          <label>Priority</label>
+
+          <Field
+              component={renderRadio}
+              label="lowest"
+              name="diversity_average_priority"
+              radioValue={1}
           />
           <Field
-            component={Form.Input}
-            label="Last name"
-            name="lastName"
-            placeholder="Last name"
+              component={renderRadio}
+              label="low"
+              name="diversity_average_priority"
+              radioValue={2}
           />
           <Field
-            component={renderSelect}
-            label="Gender"
-            name="gender"
-            options={[
-              { key: "m", text: "Male", value: "male" },
-              { key: "f", text: "Female", value: "female" }
-            ]}
-            placeholder="Gender"
+              component={renderRadio}
+              label="medium"
+              name="diversity_average_priority"
+              radioValue={3}
+          />
+          <Field
+              component={renderRadio}
+              label="high"
+              name="diversity_average_priority"
+              radioValue={4}
+          />
+          <Field
+              component={renderRadio}
+              label="highest"
+              name="diversity_average_priority"
+              radioValue={5}
+          />
+        </Form.Group>
+
+
+        <Form.Group inline>
+          <Field
+              component={renderCheckbox}
+              label="Gender: Male"
+              name="diversity_gender_male"
+          />
+          <label>Priority</label>
+
+          <Field
+              component={renderRadio}
+              label="lowest"
+              name="diversity_gender_male_priority"
+              radioValue={1}
+          />
+          <Field
+              component={renderRadio}
+              label="low"
+              name="diversity_gender_male_priority"
+              radioValue={2}
+          />
+          <Field
+              component={renderRadio}
+              label="medium"
+              name="diversity_gender_male_priority"
+              radioValue={3}
+          />
+          <Field
+              component={renderRadio}
+              label="high"
+              name="diversity_gender_male_priority"
+              radioValue={4}
+          />
+          <Field
+              component={renderRadio}
+              label="highest"
+              name="diversity_gender_male_priority"
+              radioValue={5}
           />
         </Form.Group>
 
         <Form.Group inline>
-          <label>Quantity</label>
+          <Field
+              component={renderCheckbox}
+              label="Gender: Female"
+              name="diversity_gender_female"
+          />
+          <label>Priority</label>
 
           <Field
-            component={renderRadio}
-            label="One"
-            name="quantity"
-            radioValue={1}
+              component={renderRadio}
+              label="lowest"
+              name="diversity_gender_female_priority"
+              radioValue={1}
           />
           <Field
-            component={renderRadio}
-            label="Two"
-            name="quantity"
-            radioValue={2}
+              component={renderRadio}
+              label="low"
+              name="diversity_gender_female_priority"
+              radioValue={2}
           />
           <Field
-            component={renderRadio}
-            label="Three"
-            name="quantity"
-            radioValue={3}
+              component={renderRadio}
+              label="medium"
+              name="diversity_gender_female_priority"
+              radioValue={3}
+          />
+          <Field
+              component={renderRadio}
+              label="high"
+              name="diversity_gender_female_priority"
+              radioValue={4}
+          />
+          <Field
+              component={renderRadio}
+              label="highest"
+              name="diversity_gender_female_priority"
+              radioValue={5}
           />
         </Form.Group>
 
-        <Field
-          component={renderTextArea}
-          label="About"
-          name="about"
-          placeholder="Tell us more about you..."
-        />
+        <Form.Group inline>
+          <Field
+              component={renderCheckbox}
+              label="Location: Home"
+              name="diversity_location_home"
+          />
+          <label>Priority</label>
 
-        <Field
-          component={renderCheckbox}
-          label="I agree to the Terms and Conditions"
-          name="isAgreed"
-        />
+          <Field
+              component={renderRadio}
+              label="lowest"
+              name="diversity_location_home_priority"
+              radioValue={1}
+          />
+          <Field
+              component={renderRadio}
+              label="low"
+              name="diversity_location_home_priority"
+              radioValue={2}
+          />
+          <Field
+              component={renderRadio}
+              label="medium"
+              name="diversity_location_home_priority"
+              radioValue={3}
+          />
+          <Field
+              component={renderRadio}
+              label="high"
+              name="diversity_location_home_priority"
+              radioValue={4}
+          />
+          <Field
+              component={renderRadio}
+              label="highest"
+              name="diversity_location_home_priority"
+              radioValue={5}
+          />
+        </Form.Group>
+        <Form.Group inline>
+          <Field
+              component={renderCheckbox}
+              label="Location: Online"
+              name="diversity_location_online"
+          />
+          <label>Priority</label>
+
+          <Field
+              component={renderRadio}
+              label="lowest"
+              name="diversity_location_online_priority"
+              radioValue={1}
+          />
+          <Field
+              component={renderRadio}
+              label="low"
+              name="diversity_location_online_priority"
+              radioValue={2}
+          />
+          <Field
+              component={renderRadio}
+              label="medium"
+              name="diversity_location_online_priority"
+              radioValue={3}
+          />
+          <Field
+              component={renderRadio}
+              label="high"
+              name="diversity_location_online_priority"
+              radioValue={4}
+          />
+          <Field
+              component={renderRadio}
+              label="highest"
+              name="diversity_location_online_priority"
+              radioValue={5}
+          />
+        </Form.Group>
+
+
+        <Header as='h3'>types that should be together</Header>
+        <Form.Group inline>
+          <Field
+              component={renderSelect}
+              label="Gender: Male"
+              name="together_gender_male"
+              options={[
+                { key: "1", text: "1", value: "1" },
+                { key: "2", text: "2", value: "2" },
+                { key: "3", text: "3", value: "3" },
+                { key: "4", text: "4", value: "4" },
+                { key: "5", text: "5", value: "5" },
+                { key: "6", text: "6", value: "6" },
+                { key: "7", text: "7", value: "7" },
+                { key: "8", text: "8", value: "8" },
+                { key: "9", text: "9", value: "9" },
+                { key: "10", text: "10", value: "10" },
+              ]}
+              placeholder="Minimum to be together"
+          />
+          <label>Priority</label>
+
+          <Field
+              component={renderRadio}
+              label="lowest"
+              name="together_gender_male_priority"
+              radioValue={1}
+          />
+          <Field
+              component={renderRadio}
+              label="low"
+              name="together_gender_male_priority"
+              radioValue={2}
+          />
+          <Field
+              component={renderRadio}
+              label="medium"
+              name="together_gender_male_priority"
+              radioValue={3}
+          />
+          <Field
+              component={renderRadio}
+              label="high"
+              name="together_gender_male_priority"
+              radioValue={4}
+          />
+          <Field
+              component={renderRadio}
+              label="highest"
+              name="together_gender_male_priority"
+              radioValue={5}
+          />
+        </Form.Group>
+        <Form.Group inline>
+          <Field
+              component={renderSelect}
+              label="Gender: Female"
+              name="together_gender_female"
+              options={[
+                { key: "1", text: "1", value: "1" },
+                { key: "2", text: "2", value: "2" },
+                { key: "3", text: "3", value: "3" },
+                { key: "4", text: "4", value: "4" },
+                { key: "5", text: "5", value: "5" },
+                { key: "6", text: "6", value: "6" },
+                { key: "7", text: "7", value: "7" },
+                { key: "8", text: "8", value: "8" },
+                { key: "9", text: "9", value: "9" },
+                { key: "10", text: "10", value: "10" },
+              ]}
+              placeholder="Minimum to be together"
+          />
+          <label>Priority</label>
+
+          <Field
+              component={renderRadio}
+              label="lowest"
+              name="together_gender_female_priority"
+              radioValue={1}
+          />
+          <Field
+              component={renderRadio}
+              label="low"
+              name="together_gender_female_priority"
+              radioValue={2}
+          />
+          <Field
+              component={renderRadio}
+              label="medium"
+              name="together_gender_female_priority"
+              radioValue={3}
+          />
+          <Field
+              component={renderRadio}
+              label="high"
+              name="together_gender_female_priority"
+              radioValue={4}
+          />
+          <Field
+              component={renderRadio}
+              label="highest"
+              name="together_gender_female_priority"
+              radioValue={5}
+          />
+        </Form.Group>
+        <Form.Group inline>
+          <Field
+              component={renderSelect}
+              label="Location: Home"
+              name="together_location_home"
+              options={[
+                { key: "1", text: "1", value: "1" },
+                { key: "2", text: "2", value: "2" },
+                { key: "3", text: "3", value: "3" },
+                { key: "4", text: "4", value: "4" },
+                { key: "5", text: "5", value: "5" },
+                { key: "6", text: "6", value: "6" },
+                { key: "7", text: "7", value: "7" },
+                { key: "8", text: "8", value: "8" },
+                { key: "9", text: "9", value: "9" },
+                { key: "10", text: "10", value: "10" },
+              ]}
+              placeholder="Minimum to be together"
+          />
+          <label>Priority</label>
+
+          <Field
+              component={renderRadio}
+              label="lowest"
+              name="together_location_home_priority"
+              radioValue={1}
+          />
+          <Field
+              component={renderRadio}
+              label="low"
+              name="together_location_home_priority"
+              radioValue={2}
+          />
+          <Field
+              component={renderRadio}
+              label="medium"
+              name="together_location_home_priority"
+              radioValue={3}
+          />
+          <Field
+              component={renderRadio}
+              label="high"
+              name="together_location_home_priority"
+              radioValue={4}
+          />
+          <Field
+              component={renderRadio}
+              label="highest"
+              name="together_location_home_priority"
+              radioValue={5}
+          />
+        </Form.Group>
+        <Form.Group inline>
+          <Field
+              component={renderSelect}
+              label="Location: Online"
+              name="together_location_Online"
+              options={[
+                { key: "1", text: "1", value: "1" },
+                { key: "2", text: "2", value: "2" },
+                { key: "3", text: "3", value: "3" },
+                { key: "4", text: "4", value: "4" },
+                { key: "5", text: "5", value: "5" },
+                { key: "6", text: "6", value: "6" },
+                { key: "7", text: "7", value: "7" },
+                { key: "8", text: "8", value: "8" },
+                { key: "9", text: "9", value: "9" },
+                { key: "10", text: "10", value: "10" },
+              ]}
+              placeholder="Minimum to be together"
+          />
+          <label>Priority</label>
+
+          <Field
+              component={renderRadio}
+              label="lowest"
+              name="together_location_online_priority"
+              radioValue={1}
+          />
+          <Field
+              component={renderRadio}
+              label="low"
+              name="together_location_online_priority"
+              radioValue={2}
+          />
+          <Field
+              component={renderRadio}
+              label="medium"
+              name="together_location_online_priority"
+              radioValue={3}
+          />
+          <Field
+              component={renderRadio}
+              label="high"
+              name="together_location_online_priority"
+              radioValue={4}
+          />
+          <Field
+              component={renderRadio}
+              label="highest"
+              name="together_location_online_priority"
+              radioValue={5}
+          />
+        </Form.Group>
+
+
+
+
+
+
+
+        {/*<Form.Group widths="equal">*/}
+        {/*  <Field*/}
+        {/*    component={Form.Input}*/}
+        {/*    label="First name"*/}
+        {/*    name="firstName"*/}
+        {/*    placeholder="First name"*/}
+        {/*  />*/}
+        {/*  <Field*/}
+        {/*    component={Form.Input}*/}
+        {/*    label="Last name"*/}
+        {/*    name="lastName"*/}
+        {/*    placeholder="Last name"*/}
+        {/*  />*/}
+        {/*  <Field*/}
+        {/*    component={renderSelect}*/}
+        {/*    label="Gender"*/}
+        {/*    name="gender"*/}
+        {/*    options={[*/}
+        {/*      { key: "m", text: "Male", value: "male" },*/}
+        {/*      { key: "f", text: "Female", value: "female" }*/}
+        {/*    ]}*/}
+        {/*    placeholder="Gender"*/}
+        {/*  />*/}
+        {/*</Form.Group>*/}
+
+        {/*<Form.Group inline>*/}
+        {/*  <label>Quantity</label>*/}
+
+        {/*  <Field*/}
+        {/*    component={renderRadio}*/}
+        {/*    label="One"*/}
+        {/*    name="quantity"*/}
+        {/*    radioValue={1}*/}
+        {/*  />*/}
+        {/*  <Field*/}
+        {/*    component={renderRadio}*/}
+        {/*    label="Two"*/}
+        {/*    name="quantity"*/}
+        {/*    radioValue={2}*/}
+        {/*  />*/}
+        {/*  <Field*/}
+        {/*    component={renderRadio}*/}
+        {/*    label="Three"*/}
+        {/*    name="quantity"*/}
+        {/*    radioValue={3}*/}
+        {/*  />*/}
+        {/*</Form.Group>*/}
+
+        {/*<Field*/}
+        {/*  component={renderTextArea}*/}
+        {/*  label="About"*/}
+        {/*  name="about"*/}
+        {/*  placeholder="Tell us more about you..."*/}
+        {/*/>*/}
+
+        {/*<Field*/}
+        {/*  component={renderCheckbox}*/}
+        {/*  label="I agree to the Terms and Conditions"*/}
+        {/*  name="isAgreed"*/}
+        {/*/>*/}
 
         <Form.Group inline>
           <Form.Button primary>Submit</Form.Button>
